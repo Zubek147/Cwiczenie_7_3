87e20e2f57b3e6b14e1ce81ab6948ec9971312f9
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-09-16 21:49:27 +0200

    Add def fo BaseContact

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ from faker import Faker
 import random
 
 fake = Faker()
+
+
+class BaseContact:
+    def __init__(self, imie, nazwisko, telefon, email):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.telefon = telefon
+        self.email = email
+
+    def contact(self):
+        print(f"Wybieram numer {self.telefon} i dzwonie do {self.imie} {self.nazwisko}")
+
+    @property
+    def label_length(self):
+        return len(self.imie + " " + self.nazwisko)
+    
